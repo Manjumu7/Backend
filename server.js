37b34ca53eb0394f4918a5cb95ffaf28ebf19970
Manjunath Madar <manjunathmu7@gmail.com>
2026-01-20 08:29:50 -0800

    added home page

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require("express")
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send("this this the main page")
 })
+
+app.get("/home",(req,res)=>{
+    res.send("this is home page")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("this is about page")
+})
+
+
 
 app.listen(3000)  //  server  start
